5778583e3b57425d8ff54c3ea6f1b09116c46d14
Juste231 <avademejustelelouch@gmail.com>
2024-12-04 15:46:50 +0100

    mail

--- resources/views/emails/tache_assigne.blade.php
+++ resources/views/emails/tache_assigne.blade.php
@@ -8,7 +8,6 @@
     <p>Une nouvelle tâche vous a été assignée :</p>
     <p><strong>Titre :</strong> {{ $task->titre }}</p>
     <p><strong>Description :</strong> {{ $task->description }}</p>
-    <p><strong>Date Limite :</strong> {{ \Carbon\Carbon::parse($task->date_limite)->format('d/m/Y') }}</p>
     <p>Merci de consulter vos tâches dans l'application.</p>
 </body>
 </html>
